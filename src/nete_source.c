8ceff2e586375a56e7c8e4408b84785090a549b8
ashe-cuena <69272643+ashe-cuena@users.noreply.github.com>
2021-07-14 11:13:48 +0200

    Add files via upload

--- src/nete_source.c
+++ src/nete_source.c
@@ -1,5 +1,21 @@
-//New Era Text Editor (NETE)
-//Copyright (c) Tinashe Gwena 2021
+/*
+New Era Text Editor (NETE), A Visual Diagrammatic Text Editor
+Copyright (C) 2021 Tinashe Gwena
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 
 
